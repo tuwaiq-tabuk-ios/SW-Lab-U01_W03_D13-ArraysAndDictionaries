bb4e96e3d71988df9a8fae1a1f1af93888a51057
Bushra alatwi <bushraalatwi@Bushras-MacBook-Air.local>
2021-10-20 23:53:50 +0300

    completed

--- Collections/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
+++ Collections/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
@@ -7,19 +7,63 @@
 
 //:  Using subscripting syntax to add April to the collection with a value of 30. Print the dictionary.
 
-
 //:  It's a leap year! Update the number of days in February to 29 using the `updateValue(_:, forKey:)` method. Print the dictionary.
-
 
 //:  Use if-let syntax to retrieve the number of days under "January". If the value is there, print "January has 31 days", where 31 is the value retrieved from the dictionary.
 
 
 //:  Given the following arrays, create a new [String : [String]] dictionary. `shapesArray` should use the key "Shapes" and `colorsArray` should use the key "Colors". Print the resulting dictionary.
 
-
 //:  Print the last element of `colorsArray`, accessing it through the dictionary you've created. You'll have to use if-let syntax or the force unwrap operator to unwrap what is returned from the dictionary before you can access an element of the array.
+
 
 
 /*:
 [Previous](@previous)  |  page 3 of 4  |  [Next: App Exercise - Pacing](@next)
  */
+print("\n\n- - - - - Exercice1 - - - - -")
+
+
+var daysInMonths: [String: Int] = ["January": 31, "February": 28, "March": 31]
+print(daysInMonths)
+
+
+
+print("\n\n- - - - - Exercice2 - - - - -")
+
+
+daysInMonths["April"] = 30
+print(daysInMonths)
+
+
+
+print("\n\n- - - - - Exercice3 - - - - -")
+
+
+daysInMonths.updateValue(29, forKey: "February")
+print(daysInMonths)
+
+
+print("\n\n- - - - - Exercice4 - - - - -")
+
+
+if let daysInJan = daysInMonths["January"] {
+  print("January has \(daysInJan) days")
+}
+
+
+
+print("\n\n- - - - - Exercice5 - - - - -")
+
+
+let shapesArray = ["Colors","Square", "Triangle"]
+let colorsArray = ["Red", "Green", "Blue"]
+var shapesAndColors: [String: [String]] = ["Shapes": shapesArray, "Colors": colorsArray]
+print(shapesArray)
+
+print("\n\n- - - - - Exercice6 - - - - -")
+
+
+if let colors = shapesAndColors["Colors"] {
+  print(colors[2])
+}
