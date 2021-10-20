14c72f97caf8473f090a74038bc16992912228eb
Aisha Ali <aisha@Aishas-MacBook-Pro.local>
2021-10-20 21:04:58 +0300

    Complete Task

--- Collections/Lab - Collections.playground/Pages/2. App Exercise - Activity Challenge.xcplaygroundpage/Contents.swift
+++ Collections/Lab - Collections.playground/Pages/2. App Exercise - Activity Challenge.xcplaygroundpage/Contents.swift
@@ -7,19 +7,42 @@
  
  Using arrays of type `String`, create at least two lists, one for walking challenges, and one for running challenges. Each should have at least two challenges and should be initialized using an array literal. Feel free to create more lists for different activities.
  */
+var running = [String]()
+var climbing = [String]()
+var weight = [String]()
+var rope = [String]()
+
+
+running += ["Run 10 miles per day","Run 700 miles per week"]
+climbing += ["Climbing Mountain for 3 hours","Climbing 6 hours in weekend"]
+weight += ["Weight training for a hour","7 hours for Weight training"]
+rope += ["Rope jumping challenge","Chloe's Rope challlenge"]
 
 
 //:  In your app you want to show all of these lists on the same screen grouped into sections. Create a `challenges` array that holds each of the lists you have created (it will be an array of arrays). Using `challenges`, print the first element in the second challenge list.
+var challenges = [[String]]()
+challenges += [running,climbing,weight,rope]
 
+print(challenges[0][1])
 
 //:  All of the challenges will reset at the end of the month. Use the `removeAll` to remove everything from `challenges`. Print `challenges`.
-
-
+challenges.removeAll()
+print(challenges)
 //:  Create a new array of type `String` that will represent challenges a user has committed to instead of available challenges. It can be an empty array or have a few items in it.
-
+var showChallenge = [String]()
+showChallenge += ["Climbing Mountain for 3 hours"
+                  ,"Run 5 times a week"
+                  ,"Rope jumping challenge"
+                  ,"Chloe's summer challlenge"]
 
 //:  Write an if statement that will use `isEmpty` to check if there is anything in the array. If there is not, print a statement asking the user to commit to a challenge. Add an else-if statement that will print "The challenge you have chosen is <INSERT CHOSEN CHALLENGE>" if the array count is exactly 1. Then add an else statement that will print "You have chosen multiple challenges."
-
+if showChallenge.isEmpty {
+  print("Please select challenge")
+}else if showChallenge.count == 1 {
+    print("The challenge you have chosen is : \(showChallenge[0])")
+}else{
+  print("You have chosen multiple challenges.")
+}
 
 /*:
 [Previous](@previous)  |  page 2 of 4  |  [Next: Exercise - Dictionaries](@next)
--- Collections/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
+++ Collections/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
@@ -3,22 +3,33 @@
 
  Create a variable `[String: Int]` dictionary that can be used to look up the number of days in a particular month. Use a dictionary literal to initialize it with January, February, and March. January contains 31 days, February has 28, and March has 31. Print the dictionary.
  */
-
+var dictionaries: [String: Int] = ["January":31, "Febuary":28, "March":31]
+print(dictionaries)
 
 //:  Using subscripting syntax to add April to the collection with a value of 30. Print the dictionary.
-
-
+dictionaries ["April"] =  30
+print(dictionaries)
 //:  It's a leap year! Update the number of days in February to 29 using the `updateValue(_:, forKey:)` method. Print the dictionary.
-
+let oldValue = dictionaries.updateValue(29, forKey:"February")
+print(dictionaries)
 
 //:  Use if-let syntax to retrieve the number of days under "January". If the value is there, print "January has 31 days", where 31 is the value retrieved from the dictionary.
-
+if let printNumber = dictionaries ["January"]{
+  print("January has \(printNumber) days")
+}
 
 //:  Given the following arrays, create a new [String : [String]] dictionary. `shapesArray` should use the key "Shapes" and `colorsArray` should use the key "Colors". Print the resulting dictionary.
-
-
+//var compineArray:[String:[String]] = []
+var shapesArray = ["Traingle","Square","Circle"]
+var colorsArray = ["Red","Blue","Yellow"]
+var compineArrays : [String : [String]] = [shapesArray[0]:colorsArray
+                                           ,shapesArray[1]:colorsArray
+                                           ,shapesArray[2]:colorsArray]
+print(compineArrays)
 //:  Print the last element of `colorsArray`, accessing it through the dictionary you've created. You'll have to use if-let syntax or the force unwrap operator to unwrap what is returned from the dictionary before you can access an element of the array.
-
+if let printColor = compineArrays [shapesArray[2]]{
+  print("ast element of colorsArray are\(printColor)")
+}
 
 /*:
 [Previous](@previous)  |  page 3 of 4  |  [Next: App Exercise - Pacing](@next)
