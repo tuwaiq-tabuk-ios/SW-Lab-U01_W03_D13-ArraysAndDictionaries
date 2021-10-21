1fd542cf881c8961707e3ecdd7266a7e74bb58ed
azooz alhwiti <azoozalhwiti@MacBook-Air-azooz.local>
2021-10-21 09:23:39 +0300

    completed

--- Collections/ .playground/Pages/4. App Exercises - Pacing.xcplaygroundpage/Contents.swift
+++ Collections/ .playground/Pages/4. App Exercises - Pacing.xcplaygroundpage/Contents.swift
@@ -7,19 +7,25 @@
 
  Create a dictionary `paces` of type [String: Double] and assign it a dictionary literal with "Easy", "Medium", and "Fast" keys corresponding to values of 10.0, 8.0, and 6.0. These numbers correspond to mile pace in minutes. Print the dictionary.
  */
-
+//var Paces = ["Easy": 6.0 , "Medium" : 8.0 ,"Fast" :10.0 ]
+//print(Paces)
 
 //:  Add a new key/value pair to the dictionary. The key should be "Sprint" and the value should be 4.0. Print the dictionary.
-
+//Paces["Sprint"] = 4.0
+//print(Paces)
 
 //:  Imagine the user in question gets faster over time and decides to update his/her pacing on runs. Update the values of "Medium" and "Fast" to 7.5 and 5.8, respectively. Print the dictionary.
+//Paces.updateValue(7.5, forKey: "Medium")
+//Paces.updateValue(5.8, forKey: "Fast")
 
-
+//print(Paces)
 //:  Imagine the user in question decides not to store "Sprint" as one his/her regular paces. Remove "Sprint" from the dictionary. Print the dictionary.
-
-
+//Paces["Sprint"] = nil
+//print(Paces)
 //:  When a user chooses a pace, you want the app to print a statement stating that it will keep him/her on pace. Imagine a user chooses "Medium." Accessing the value from the dictionary, print a statement saying "Okay! I'll keep you at a <INSERT PACE VALUE HERE> minute mile pace."
-
+//if Paces["Medium"] != nil{
+   // print("Okay! I'll keep you ata \(Paces["Medium"]!) minute mile pace")
+//}
 
 /*:
  _Copyright © 2021 Apple Inc._
@@ -32,3 +38,16 @@
  
 [Previous](@previous)  |  page 4 of 4
  */
+var Paces = ["Easy": 6.0 , "Medium" : 8.0 ,"Fast" :10.0 ]
+print(Paces)
+Paces["Sprint"] = 4.0
+print(Paces)
+Paces.updateValue(7.5, forKey: "Medium")
+Paces.updateValue(5.8, forKey: "Fast")
+
+print(Paces)
+Paces["Sprint"] = nil
+print(Paces)
+if Paces["Medium"] != nil{
+    print("Okay! I'll keep you ata \(Paces["Medium"]!) minute mile pace")
+}
